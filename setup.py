6ceae144f39571c9ff09aa43fd105f573f5a99d3
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-07-22 12:12:30 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    README = f.read()
+
+setup(
+    name='devcmd',
+    author='Devon (Gorialis) R',
+    url='https://github.com/Cyber-Incorporated/devcmd',
+
+    license='MIT',
+    description='a developer tool for discord.py discord bots',
+    long_description=README,
+    project_urls={
+        'Code': 'https://github.com/Cyber-Incorporated/devcmd',
+        'Issue tracker': 'https://github.com/Cyber-Incorporated/devcmd/issues'
+    },
+
+    version=0.0.1,
+    packages=['devcmd'],
+    include_package_data=True,
+    install_requires=REQUIREMENTS,
+    python_requires='>=3.8.0',
+
+    extras_require=EXTRA_REQUIRES,
+
+    download_url=f'https://github.com/Cyber-Incorporated/devcmd/blob/main/dist/devcmd-1.0.0.tar.gz',
+
+    classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    ]
+)

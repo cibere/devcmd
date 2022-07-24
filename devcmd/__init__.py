78d82059aa0ecd4ad4796982e08dbf48a482b7f4
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-07-24 14:00:14 -0700

    moved stuff around

--- devcmd/__init__.py
+++ devcmd/__init__.py
@@ -12,11 +12,79 @@ import sys, traceback
 import subprocess
 from dotenv import load_dotenv
 load_dotenv()
-from infoCmd import devcmd_base
-_devcmd = devcmd_base._devcmd
 
 mystbin_client = mystbin.Client()
 VERSION = "beta-0.0.5.6"
+
+masterEmbeds = {
+    'doc':discord.Embed(
+        title="Devcmd Documentation",
+        description="https://gist.github.com/cibere/da22060df5ab6282b452e972f08d269b",
+        color=discord.Color.blue()
+    ),
+    'git':discord.Embed(
+        title="Devcmd Github",
+        description="""(Stable/Main Version)[https://github.com/cibere/devcmd]\n(Development/Beta Version)[https://github.com/cibere/devcmd/tree/beta]""",
+        color=discord.Color.blue()
+    ),
+    'color':discord.Embed(
+        title="Embed Color Code",
+        description="""
+> **Blue:**
+> __Informational Embed__
+
+> **Green**
+> __Successfull Embed__
+
+> **Red**
+> __An Error Embed__
+
+> **Orange**
+> __A Error has been sent to your dms Embed__
+""",
+        color=discord.Color.blue()
+    )
+}
+
+masterOptions = {
+    'doc':[
+            discord.SelectOption(label='Docs', description='Gives you the devcmd docs', value='doc', default=True),
+            discord.SelectOption(label='Github', description='Gives you the devcmd github page', value='git'),
+            discord.SelectOption(label='Embed Colors', description='Gives you info about the coloring of embeds', value='color'),
+    ],
+    'git':[
+            discord.SelectOption(label='Docs', description='Gives you the devcmd docs', value='doc'),
+            discord.SelectOption(label='Github', description='Gives you the devcmd github page', value='git', default=True),
+            discord.SelectOption(label='Embed Colors', description='Gives you info about the coloring of embeds', value='color'),
+    ],
+    'color':[
+            discord.SelectOption(label='Docs', description='Gives you the devcmd docs', value='doc'),
+            discord.SelectOption(label='Github', description='Gives you the devcmd github page', value='git'),
+            discord.SelectOption(label='Embed Colors', description='Gives you info about the coloring of embeds', value='color', default=True),
+    ],
+    'normal':[
+            discord.SelectOption(label='Docs', description='Gives you the devcmd docs', value='doc'),
+            discord.SelectOption(label='Github', description='Gives you the devcmd github page', value='git'),
+            discord.SelectOption(label='Embed Colors', description='Gives you info about the coloring of embeds', value='color'),
+    ],
+}
+
+class infoDropdown(discord.ui.Select):
+    def __init__(self, owner, options):
+        self.owner = owner
+
+        super().__init__(placeholder='Choose a option', min_values=1, max_values=1, options=options)
+
+    async def callback(self, interaction: discord.Interaction):
+        if interaction.user.id != self.owner.id:
+            return await interaction.response.send_message(f"This is for my owner(s) only.", ephemeral=True)
+        await interaction.response.edit_message(embed=masterEmbeds[self.values[0]], view=infoDropdown(self.owner, masterOptions[self.values[0]]))
+
+class infoDropdownView(discord.ui.View):
+    def __init__(self, owner, options):
+        super().__init__(timeout=None)
+
+        self.add_item(infoDropdown(owner, options))
 
 class CodeBlock(commands.Converter):
     async def convert(self,ctx, block:str):
@@ -44,7 +112,23 @@ class RedirectedStdout:
     def __str__(self):
         return self._string_io.getvalue()
 
-class devcmd(devcmd_base):
+class devcmd(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.group(hidden=True, invoke_without_command=True, name="devcmd", aliases=['dev', 'dc'])
+    @is_owner()
+    async def _devcmd(self, ctx, *, extra_args=None):
+        if extra_args == None:
+            await ctx.send("Invalid Syntax")
+        else:
+            raise discord.ext.commands.CommandNotFound(f'Command "{ctx.invoked_with} {extra_args}" is not found')
+
+    @_devcmd.command(name="info", alises=['about', 'github', 'docs'])
+    @is_owner()
+    async def _dc_info(self, ctx):
+        em = discord.Embed(title="Please make a selection", color=discord.Color.blue())
+        await ctx.send(embed=em, view=infoDropdownView(ctx.author, masterOptions['normal']))
 
     @_devcmd.command(aliases=['logs'], name="audit")
     @is_owner()

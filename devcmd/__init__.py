d51c9a42a2fa29606eb4d44b8dfccefe8ada950a
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-07-28 20:08:16 -0700

    Update __init__.py

--- devcmd/__init__.py
+++ devcmd/__init__.py
@@ -16,7 +16,7 @@ load_dotenv()
 disallowedLibs = ['requests', 'urllib', 'time', 'ImageMagick', 'PIL', 'sqlite3', 'postgres', "easy_pil", 'json']
 
 mystbin_client = mystbin.Client()
-VERSION = "beta-1.0.1.12"
+VERSION = "beta-1.0.1.13"
 url = "https://github.com/cibere/devcmd@beta"
 
 class infoCmd:

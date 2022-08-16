7b6217f7adab08d3b7fb68d0cfd649ecd3a39d28
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-08-16 10:59:57 -0700

    updated version due to the amount of recent commits

--- devcmd/core.py
+++ devcmd/core.py
@@ -16,7 +16,7 @@ load_dotenv()
 disallowedLibs = ['requests', 'urllib', 'time', 'ImageMagick', 'PIL', 'sqlite3', 'postgres', "easy_pil", 'json']
 
 mystbin_client = mystbin.Client()
-VERSION = "2.0.0"
+VERSION = "2.1.0"
 url = "https://github.com/cibere/devcmd"
 
 class infoCmd:

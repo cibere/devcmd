2a969dfdb4df194ad5928c5f4d438180fd646e12
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-09-17 18:14:22 -0700

    updated version

--- devcmd/core.py
+++ devcmd/core.py
@@ -20,7 +20,7 @@ disallowedLibs = ['requests', 'urllib', 'time', 'ImageMagick', 'PIL', 'sqlite3',
 
 mystbin_client = mystbin.Client()
 TOKEN_REGEX = re.compile(r'[a-zA-Z0-9_-]{23,28}\.[a-zA-Z0-9_-]{6,7}\.[a-zA-Z0-9_-]{27,}')
-VERSION = "BETA-3.3.2"
+VERSION = "BETA-3.3.3"
 url = "https://github.com/cibere/devcmd@beta"
 
 class infoCmd:

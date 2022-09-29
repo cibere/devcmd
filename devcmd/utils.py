8ff92c5964f5f31af85c1292b30476781a58e10c
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-09-28 19:59:37 -0700

    renamed paginator to use read-able names

--- devcmd/utils.py
+++ devcmd/utils.py
@@ -5,33 +5,34 @@ class Paginator(discord.ui.View):
         self.user = user
         self.pages = pages
         self.current_page = 0
-        super().__init__(timeout=None)
-        self._dc_hb_num.label = f"{self.current_page + 1}/{len(self.pages)}"
+        self.indexButton.label = f"{self.current_page + 1}/{len(self.pages)}"
         if len(self.pages) == 1:
-            self._dc_hb_right.disabled=True
+            self.rightButton.disabled=True
+        super().__init__(timeout=None)
+
+    async def interaction_check(self, interaction: discord.Interaction) -> bool:
+        if self.user.id != interaction.user.id:
+            await interaction.response.send_message(f"Your not {self.user.mention}... are you?", ephemeral=True)
+            return False
 
     @discord.ui.button(emoji='⬅️', style=discord.ButtonStyle.blurple, disabled=True)
-    async def _dc_hb_left(self, interaction: discord.Interaction, button: discord.ui.Button):
-        if self.user.id != interaction.user.id:
-            return await interaction.response.send_message(f"Your not {self.user.mention}... are you?", ephemeral=True)
+    async def leftButton(self, interaction: discord.Interaction, button: discord.ui.Button):
         if self.current_page - 1 == 0:
-            self._dc_hb_left.disabled = True
-        self._dc_hb_right.disabled = False
+            self.leftButton.disabled = True
+        self.rightButton.disabled = False
         self.current_page -= 1
-        self._dc_hb_num.label = f"{self.current_page + 1}/{len(self.pages)}"
+        self.indexButton.label = f"{self.current_page + 1}/{len(self.pages)}"
         await interaction.response.edit_message(embed=self.pages[self.current_page], view=self)
 
     @discord.ui.button(label=f'1', style=discord.ButtonStyle.gray, disabled=True)
-    async def _dc_hb_num(self, interaction: discord.Interaction, button: discord.ui.Button):
+    async def indexButton(self, interaction: discord.Interaction, button: discord.ui.Button):
         pass
 
     @discord.ui.button(emoji='➡️', style=discord.ButtonStyle.blurple)
-    async def _dc_hb_right(self, interaction: discord.Interaction, button: discord.ui.Button):
-        if self.user.id != interaction.user.id:
-            return await interaction.response.send_message(f"Your not {self.user.mention}... are you?", ephemeral=True)
+    async def rightButton(self, interaction: discord.Interaction, button: discord.ui.Button):
         if self.current_page + 1 == len(self.pages) - 1:
-            self._dc_hb_right.disabled = True
-        self._dc_hb_left.disabled = False
+            self.rightButton.disabled = True
+        self.leftButton.disabled = False
         self.current_page += 1
-        self._dc_hb_num.label = f"{self.current_page + 1}/{len(self.pages)}"
+        self.indexButton.label = f"{self.current_page + 1}/{len(self.pages)}"
         await interaction.response.edit_message(embed=self.pages[self.current_page], view=self)

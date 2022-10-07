4aa6538d431ae32bb255c9f6b49068c25976eff4
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-10-07 09:54:19 -0700

    fixed a very stupid mistake

--- devcmd/core.py
+++ devcmd/core.py
@@ -22,7 +22,7 @@ disallowedLibs = ['requests', 'urllib', 'time', 'ImageMagick', 'PIL', 'sqlite3',
 
 mystbin_client = mystbin.Client()
 TOKEN_REGEX = re.compile(r'[a-zA-Z0-9_-]{23,28}\.[a-zA-Z0-9_-]{6,7}\.[a-zA-Z0-9_-]{27,}')
-VERSION = "BETA-3.3.15"
+VERSION = "BETA-3.3.16"
 url = "https://github.com/cibere/devcmd@beta"
 
 class infoCmd:
@@ -317,7 +317,7 @@ class devcmd(commands.Cog):
         function = function.splitlines()
         x = function[-1].removeprefix("    ")
         if not x.startswith("print") and not x.startswith("return"):
-            function.pop(function[-1])
+            function.pop(function.index(function[-1]))
             function.append(f"    return {x}")
         function = '\n'.join(function)
         with RedirectedStdout() as otp:

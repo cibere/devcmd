cf7ed110ffdd7fe869c1406d2c00655c7d15a077
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-10-07 10:30:08 -0700

    updated version

--- devcmd/core.py
+++ devcmd/core.py
@@ -21,7 +21,7 @@ import statistics
 disallowedLibs = ['requests', 'urllib', 'time', 'ImageMagick', 'PIL', 'sqlite3', 'postgres', "easy_pil", 'json']
 
 TOKEN_REGEX = re.compile(r'[a-zA-Z0-9_-]{23,28}\.[a-zA-Z0-9_-]{6,7}\.[a-zA-Z0-9_-]{27,}')
-VERSION = "BETA-3.3.23"
+VERSION = "BETA-3.3.24"
 url = "https://github.com/cibere/devcmd@beta"
 
 class infoCmd:

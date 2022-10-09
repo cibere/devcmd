3047fa9443e0d06d95db7ab3095e547c488e3eba
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-10-08 19:36:10 -0700

    modified how it gets the version

--- devcmd/__init__.py
+++ devcmd/__init__.py
@@ -1,5 +1,22 @@
 from .core import VERSION, devcmd
 from .utils import Paginator
 
+if VERSION.startswith('BETA'):
+    # append version identifier based on commit count
+    try:
+        import subprocess
+        p = subprocess.Popen(['git', 'rev-list', '--count', 'HEAD'],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if out:
+            VERSION += out.decode('utf-8').strip()
+        p = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if out:
+            VERSION += '+g' + out.decode('utf-8').strip()
+    except Exception:
+        pass
+
 async def setup(bot):
     await bot.add_cog(devcmd(bot))

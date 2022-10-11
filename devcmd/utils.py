a374aaaa1ecb889df7c0b115f3a15db33f4e0e51
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-10-11 16:33:07 -0700

    added custom_ids to the paginator buttons

--- devcmd/utils.py
+++ devcmd/utils.py
@@ -14,8 +14,9 @@ class Paginator(discord.ui.View):
         if self.user.id != interaction.user.id:
             await interaction.response.send_message(f"Your not {self.user.mention}... are you?", ephemeral=True)
             return False
+        return True
 
-    @discord.ui.button(emoji='⬅️', style=discord.ButtonStyle.blurple, disabled=True)
+    @discord.ui.button(emoji='⬅️', style=discord.ButtonStyle.blurple, disabled=True, custom_id="devcmd_paginator:left")
     async def leftButton(self, interaction: discord.Interaction, button: discord.ui.Button):
         if self.current_page - 1 == 0:
             self.leftButton.disabled = True
@@ -24,11 +25,11 @@ class Paginator(discord.ui.View):
         self.indexButton.label = f"{self.current_page + 1}/{len(self.pages)}"
         await interaction.response.edit_message(embed=self.pages[self.current_page], view=self)
 
-    @discord.ui.button(label=f'1', style=discord.ButtonStyle.gray, disabled=True)
+    @discord.ui.button(label=f'1', style=discord.ButtonStyle.gray, disabled=True, custom_id="devcmd_paginator:index")
     async def indexButton(self, interaction: discord.Interaction, button: discord.ui.Button):
         pass
 
-    @discord.ui.button(emoji='➡️', style=discord.ButtonStyle.blurple)
+    @discord.ui.button(emoji='➡️', style=discord.ButtonStyle.blurple, custom_id="devcmd_paginator:right")
     async def rightButton(self, interaction: discord.Interaction, button: discord.ui.Button):
         if self.current_page + 1 == len(self.pages) - 1:
             self.rightButton.disabled = True

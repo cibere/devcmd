b6c14150dfd5672a1b3c1e1e28e2f4105478d408
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-10-14 20:57:18 -0700

    Update core.py

--- devcmd/core.py
+++ devcmd/core.py
@@ -13,7 +13,7 @@ import sys, traceback, aiohttp
 import subprocess
 from dotenv import load_dotenv
 load_dotenv()
-from .utils import EmbedPaginator, CodeBlockConvertor
+from .__init__ import EmbedPaginator, CodeBlockConvertor
 import time
 import statistics
 

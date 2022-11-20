ad9f2a91e624155a7ad0b8a4d8e2289e92d3dfa5
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-11-20 12:34:09 -0800

    added custom send methods

--- devcmd/sections/base_section.py
+++ devcmd/sections/base_section.py
@@ -1,9 +1,40 @@
+import datetime
+
+import discord
 from discord.ext import commands
 
 
 @commands.is_owner()
 class BaseSection(commands.Cog):
-    pass
+    async def send_error(self, messageable, message: str) -> None:
+        em = discord.Embed(
+            title="Error",
+            description=message,
+            color=discord.Color.red(),
+            timestamp=datetime.datetime.utcnow(),
+        )
+        await self.send_message(messageable, em)
+
+    async def send_success(self, messageable, message: str) -> None:
+        em = discord.Embed(
+            title="Success",
+            description=message,
+            color=discord.Color.green(),
+            timestamp=datetime.datetime.utcnow(),
+        )
+        await self.send_message(messageable, em)
+
+    async def send_info(self, messageable, title: str, message: str) -> None:
+        em = discord.Embed(
+            title=title,
+            description=message,
+            color=discord.Color.blue(),
+            timestamp=datetime.datetime.utcnow(),
+        )
+        await self.send_message(messageable, em)
+
+    async def send_message(self, messageable, embed: discord.Embed) -> None:
+        await messageable.send(embed=embed)
 
 
 def command(*, name: str, description: str, aliases: list[str] = []):

56589b7d226e5d6c0541418a48c3a5f5d1b77e9b
cibere <71997063+Cyber-Incorporated@users.noreply.github.com>
2022-11-20 13:02:29 -0800

    made RedirectedStdout async

--- devcmd/utils.py
+++ devcmd/utils.py
@@ -14,12 +14,12 @@ class RedirectedStdout:
         self._stdout = None
         self._string_io = None
 
-    def __enter__(self):
+    async def __aenter__(self):
         self._stdout = sys.stdout
         sys.stdout = self._string_io = StringIO()
         return self
 
-    def __exit__(self, type, value, traceback):
+    async def __aexit__(self, type, value, traceback):
         sys.stdout = self._stdout
 
     def __str__(self):
